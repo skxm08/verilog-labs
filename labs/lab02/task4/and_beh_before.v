041063b3eece115b9b10f1aca37c4970805b4e3d
skxm08 <f20240805@pilani.bits-pilani.ac.in>
2026-09-19 14:02:44 +0000

    lab02 task4

---
+++ labs/lab02/task4/and_beh_before.v
@@ -0,0 +1,10 @@
+module and_beh_before (
+    input a,
+    input b,
+    output reg y 
+);
+
+    always @(*) begin 
+        #3 y = a&b;
+    end
+endmodule 
---
+++ labs/lab02/task4/and_df.v
@@ -0,0 +1,8 @@
+module and_df(
+    input a,
+    input b,
+    output y
+);
+
+    assign #3 y= a&b;
+endmodule 
